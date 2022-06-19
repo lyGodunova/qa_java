caca31c128b75d794a493c09fad295a114b2e641
lygodunova <ludmila111114@mail.ru>
2022-06-19 18:10:09 +0300

    Добавление тестов к классу Animal

--- src/test/java/com/example/animalTest.java
+++ src/test/java/com/example/animalTest.java
@@ -1,4 +1,42 @@
 package com.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+@RunWith(Parameterized.class)
 public class animalTest {
+    private final String animalKind;
+    private final List<String> expected;
+
+    public animalTest (String animalKind, List<String> expected)throws Exception{
+        this.animalKind = animalKind;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters // добавили аннотацию
+    public static Object[][] getData() {
+        return new Object[][] {
+                {"Травоядное", List.of("Трава", "Различные растения")},
+                {"Хищник", List.of("Животные", "Птицы", "Рыба")},
+        };
+    }
+
+    Animal animal = new Animal();
+
+    @Test
+    public void testGetFamily(){
+        String actual = animal.getFamily();
+        String expected = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getFood() throws Exception {
+        List<String> actual= animal.getFood(animalKind);
+        Assert.assertEquals(expected, actual);
+    }
 }
